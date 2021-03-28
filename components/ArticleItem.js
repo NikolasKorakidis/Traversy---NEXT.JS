4d4458df4d9f404cba4832b66c0acea16221823d
nikolaskorakidis <korakidisn@gmail.com>
2021-03-28 17:04:21 +0200

    article

---
+++ components/ArticleItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import articleStyles from "../styles/Article.module.css";
+import Link from "next/link";
+
+export default function ArticleItem({ article }) {
+  return (
+    <Link href="/article/[id]" as={`article/${article.id}`}>
+      <h3>{article.title} &rarr;</h3>
+      <p>{article.body}</p>
+    </Link>
+  );
+}
